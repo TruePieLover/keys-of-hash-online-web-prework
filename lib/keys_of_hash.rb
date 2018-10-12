484be6d25644e64cdaff12b3a2bda57c37fb4207
Brandon Teich <brandonteich123@gmail.com>
2018-10-12 04:18:25 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
   def keys_of(arguments)
-    # code goes here
+    arr =[]
+    arguments.each do |args|
+      self.each do|key, value|
+        if value == args
+        arr << key
+        end
+      end
+    end
+    arr
+  end	  end
   end
 end
